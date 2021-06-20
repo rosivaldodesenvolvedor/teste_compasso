02d301d56408c9d9a7f5a8563ee2fbf9b43f778d
Rosivaldo <rosivaldoedeus@gmail.com>
2021-06-20 20:26:17 -0300

    subindo backgroud header

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components'
 
 export const ContainerHeader = styled.div`
-background-color: darkgrey;
+background-color: #000080;
 width: 100%;
 height: 80px;
 justify-content: flex-end;
 margin-bottom: 30px;
 margin: 0  auto;
 display:flex;
+color: white;
 `
 export const ItemHeader = styled.div`
 width: 10%;
